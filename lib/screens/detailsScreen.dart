f6689522a40b360c90b167e5c7879f834295b93f
Vici912 <victoria.fuchs@informaitk.hs-fulda.de>
2019-12-03 22:09:04 +0100

    Ergänzung der Detail Seite

--- lib/screens/detailsScreen.dart
+++ lib/screens/detailsScreen.dart
@@ -79,15 +79,130 @@ class _DetailsScreenState extends State<DetailsScreen> {
                     width: 200.0,
                   ),
                 ),
-              )
+              ),
 
-            ],
-          )
-        ],
+              Positioned(
+                top: 250.0,
+                left: 25.0,
+                right: 25.0,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(widget.foodName,
+                        style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontSize: 22.0,
+                          fontWeight: FontWeight.bold)),
+                    SizedBox(height: 20.0),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(widget.foodPrice,
+                          style: TextStyle(
+                            fontFamily: 'Montserrat',
+                              fontSize: 20.0,
+                              color: Colors.grey)),
+                        Container(
+                          height: 25.0, color: Colors.grey, width: 1.0),
+                        Container(
+                          width: 125.0,
+                          height: 40.0,
+                          decoration: BoxDecoration(
+                             borderRadius: BorderRadius.circular(17.0),
+                             color: Color(0xFF7A9BEE)),
+                           child: Row(
+                               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                               children: <Widget>[
+                                 InkWell(
+                                   onTap: () {},
+                                    child: Container(
+                                       height: 25.0,
+                                       width: 25.0,
+                                        decoration: BoxDecoration(
+                                            borderRadius: BorderRadius.circular(7.0),
+                                            color: Color(0xFF7A9BEE)),
+                                    child: Center(
+                                       child: Icon(
+                                          Icons.remove,
+                                           color: Colors.white,
+                                           size: 20.0,
+                                          ),
+                                       ),
+                                    ),
+                                  ),
+                                    Text('2',
+                                         style: TextStyle(
+                                         color: Colors.white,
+                                         fontSize: 15.0)),
+                            ],
+                          ),
+                     ),
+                     ],
+
+                    ),
+
+
+                    ],
+                     ),
+                ),
+              Positioned(
+                top: 370.0,
+                left: 30.0,
+                child: Container(
+                  height: 200.0,
+                  width: 350.0,
+                  padding: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        color: Colors.grey,
+                        style: BorderStyle.solid,
+                        width: 1.0),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+
+                  child: Column(
+                      children: <Widget>[
+                        SizedBox(width: 30.0, height: 5.0,),
+                           Text('Learning',
+                           style: TextStyle(
+                           fontFamily: 'Montserrat',
+                            color: Colors.grey,
+                           fontWeight: FontWeight.bold,
+                           fontSize: 18.0)),
+
+                         SizedBox(width: 30.0, height: 15.0,),
+                          Text('Unser Angebot erstreckt sich über alle Klassenstufen. Wir bieten einen Kurs zu Flutter an, in dem man lernt, wie man eine App erstellt. Außerdem haben wir auch Kuse in Java und C. Für Interessierte gibt es Kurse  wie man eine Webseite erstellt mit HTML und CSS.',
+
+                              style: TextStyle(
+                              fontFamily: 'Montserrat',
+                            color:Colors.grey,
+
+                             fontSize: 15.0))
+                          ],
+
+                  ),
+                ),
+              ),
+              ],
+
+
+
     ),
 
+                ],
 
+                ),
     );
+
+
+
+
+
+
+
+
+
+
 
   }
 }
